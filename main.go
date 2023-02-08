9d74ddc642baaf57cd960273df089b870c093bca
cassidy.hall94 <cassidy.hall94@gmail.com>
2023-02-08 17:24:35 +0000

    start

---
+++ main.go
@@ -0,0 +1,19 @@
+// Define the functionalities: The app will only track the dates when a user's period starts and ends. Reminders will be given 2 days before their period is set to begin, it will also need to have the ability to predict when the next period will start using the user’s data collected over time.
+
+// Choose a technology stack: The backend will be built using Golang, and the frontend will be built using JavaScript and HTML/CSS.
+
+// Design the data model: A database will be used to store the menstrual cycle data for each user. This data will be accessible and owned by the user, with proper measures in place to ensure the privacy and security of their data.
+
+// Set up the user interface: The frontend interface will be designed using JavaScript and HTML/CSS. It will include a calendar view for tracking and displaying menstrual cycle information.
+
+// Implement the functionality: The backend will be responsible for storing, retrieving, and processing the menstrual cycle data, while the frontend will be responsible for displaying the data to the user and providing an interface for users to interact with the app.
+
+// Test the app: Thorough testing should be performed on both the frontend and backend to ensure that the app works as expected and that there are no security vulnerabilities.
+
+// Deploy the app: The app will be deployed on a server, and the frontend and backend will be connected to allow for seamless communication and data transfer.
+
+package main
+
+func main() {
+	
+}
