522c78fc41530f6e705add49fa4247591a35fd1f
cassidy.hall94 <cassidy.hall94@gmail.com>
2023-03-08 13:23:09 +0000

    before indexedDB video watched

--- frontend.js
+++ frontend.js
@@ -19,7 +19,9 @@ function displayDate() {
         dates.appendChild(document.createElement("br"));
         dates.style.display = "block";
         userData = { id: userID, startDate: periodStartDate, endDate: periodEndDate };
+        createDatabase()
     });
+    console.log("savedDates: ", dates.value)
 }
 
 displayDate();
@@ -27,20 +29,28 @@ displayDate();
 const request = window.indexedDB.open("database", 2);
 
 function createDatabase() {
+    console.log("DB userData: ", userData)
     console.log("database initialising")
+    console.log("Before request: ", request)
+
     request.onerror = (event) => {
         console.error(`Database error: ${event.target.errorCode}`);
     };
 
+    console.log("hi")
+
     request.onupgradeneeded = (event) => {
+        console.log("onupgraded initialising")
         const db = event.target.result;
 
         // Create an objectStore to hold information about user
         const objectStore = db.createObjectStore("user", { keyPath: "id" });
-
+        objectStore.createIndex("period_start_date", ["startDate"], { unique: true, })
+        objectStore.createIndex("period_end_date", ["endDate"], { unique: true, })
         // Use transaction oncomplete to make sure the objectStore creation is
         // finished before adding data into it.
         objectStore.transaction.oncomplete = (event) => {
+            console.log("transaction initialising")
             // Store values in the newly created objectStore.
             const userObjectStore = db.transaction("user", "readwrite").objectStore("user");
             userObjectStore.add(userData);
@@ -53,7 +63,10 @@ function createDatabase() {
         };
     };
 
+    console.log("hru")
+
     request.onsuccess = (event) => {
+        console.log("request success: ", request)
         const db = event.target.result;
         const transaction = db.transaction(["user"], "readwrite");
         const objectStore = transaction.objectStore("user");
@@ -63,9 +76,10 @@ function createDatabase() {
         };
         request.onsuccess = (event) => {
             const user = event.target.result;
+            console.log(event.target)
             console.log("User data retrieved from database:", user);
         };
     };
+    console.log("AFTER request: ", request)
+    console.log("bye")
 }
-console.log('Before createDatabase() call');
-createDatabase();
